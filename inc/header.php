0c3d0375692118dc846a6d10e689a6fab2c1f2b7
YEINER FREYLER BONETT RODRIGUEZ <yeinerf.bonrod@educa.jcyl.es>
2026-01-28 20:26:23 +0100

    Integracion de interfaces

--- inc/header.php
+++ inc/header.php
@@ -1,16 +1,16 @@
 <!-- HEADER -->
 <header>
     <div class="logo">
-        <a href="index.php" class="logo-link">
-            <img src="images/GYA.png" alt="LuxeStay - Inicio" id="logo-img">
+        <a href="../index.php" class="logo-link">
+            <img src="../images/GYA.png" alt="LuxeStay - Inicio" id="logo-img">
         </a>
     </div>
     <nav>
-        <a href="index.php">Inicio</a>
-        <a href="views/apartamentos.php">Apartamentos</a>
-        <a href="views/reservas.php">Reservas</a>
-        <a href="views/indexLogin.php">Login</a>
-        <a href="views/busqueda.php" class="search-icon" title="Búsqueda">
+        <a href="../index.php">Inicio</a>
+        <a href="../views/indexApartamentos.php">Apartamentos</a>
+        <a href="../views/indexReservas.php">Reservas</a>
+        <a href="../views/indexLogin.php">Login</a>
+        <a href="../views/indexBusqueda.php" class="search-icon" title="Búsqueda">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
                 <circle cx="11" cy="11" r="8"></circle>
                 <path d="m21 21-4.35-4.35"></path>
